9d5b07e7c7a35d28a70e41d4cd2940e47c523aab
asghar sheikh <itasghar786@gmail.com>
2020-03-28 22:16:04 -0400

    CodeAdded

---
+++ dev/emplayeeSeeds.sql
@@ -0,0 +1,24 @@
+-- NOTE: Before seeding employees into SQL Workbench, SEED first the roles and department first.
+INSERT INTO department(name) VALUES('Engineering');
+INSERT INTO department(name) VALUES('Management');
+INSERT INTO department(name) VALUES('Marketing');
+INSERT INTO department(name) VALUES('Legal');
+INSERT INTO department(name) VALUES('Sales');
+
+INSERT INTO role(title, salary) VALUES ('Junior Developer', 50000);
+INSERT INTO role(title, salary) VALUES ('Senior Developer', 75000);
+INSERT INTO role(title, salary) VALUES ('Manager', 90000);
+INSERT INTO role(title, salary) VALUES ('Sales Lead', 40000);
+INSERT INTO role(title, salary) VALUES ('Lawyer', 65000);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Jerry","Seinfeld", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("George","Constanza", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Elaine","Benes", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Cosmo","Kramer", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Rachel","Green", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Phoebe","Boufay", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Chandler","Bing", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Joey","Tribbiani", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Ross","Gellar", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Monica","Gellar", 4);
+
---
+++ dev/employeeSchema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
+);
+
+CREATE TABLE employee (
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES department(department_id)
+)
+
+--SELECTS and JOINS the id's from the Role and Department tables together into one result.
+SELECT employee.first_name, 
+	employee.last_name,
+	role.title AS Title,
+    role.salary AS Salary,
+    department.name AS Department
+FROM employee 
+	INNER JOIN role ON employee.role_id=role.role_id
+    INNER JOIN department ON employee.role_id=department.department_id
